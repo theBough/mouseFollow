0616c18de4b66017dbbe945df0827dceeca6eedf
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-11-22 11:39:36 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let g;
 function setup() {
   createCanvas(400, 400);
-  g = new Gun(200,200,25,50);
+  g = new Gun(200,350,25,50);
 }
 
 function draw() {
   background(220);
   g.display();
+  g.update();
 }
