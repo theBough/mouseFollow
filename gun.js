b3d89ebbae795fbf4f0cae7768badf3bb36669be
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-11-22 11:33:18 -0600

    Create gun.js

---
+++ gun.js
@@ -0,0 +1,12 @@
+let imgGun;
+function Gun(x,y,w,h){
+  this.x = x;
+  this.y  =y;
+  this.w = w;
+  this.h = h;
+  imgGun = loadImage("shot0.png");
+  
+  this.display = function(){
+    image(imgGun, this.x, this.y, this.w, this.h);
+  }
+}
