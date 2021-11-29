e80eaf5a4a9be919d67743f7d94bc34633f49c3d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-11-29 11:27:23 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
 let g;
+let shooting;
 function setup() {
   createCanvas(400, 400);
-  g = new Gun(200,350,25,50);
+  g = new Gun(200,350,50,50);
 }
 
 function draw() {
   background(220);
   g.display();
   g.update();
+  
+}
+function mouseClicked(){
+  shooting = setInterval(g.shoot , 80);
 }
