df8f2c07163beaa72e70ea3b61fd46f202676bda
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-12-08 11:43:39 -0600

    Update bottle.js

--- bottle.js
+++ bottle.js
@@ -11,8 +11,5 @@ function Bottle(x,y,w,h){
   this.display = function(){
     image(bottleImage,this.x, this.y, this.w, this.h);
   }//end display
-  this.placeBottle = function(){
-    this.x = random(width - this.w);
-    this.y = random(height - this.h);
-  }//end place Bottle
+  
 }//end Gun
