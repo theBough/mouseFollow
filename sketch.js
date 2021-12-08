4e43c49965e8c1364d39e810fb202ad679228400
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-12-08 11:51:27 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,20 @@
 let g,b;
 let shooting;
 let notShooting;
+
 function setup() {
   createCanvas(400, 400);
   g = new Gun(200,350,50,50);
   b = new Bottle(200,200,30,50);
   notShooting = true;
+  rect(0, 300, width, 20 );
 }
 
 function draw() {
   background(220);
+  let c = color(210,105,30)
+  fill(c)
+  rect(0, 250, width, 20 );
   g.display();
   g.update();
   b.display();
