5aea7d1556a17eacaa4a2f723f19603fa61a0922
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-12-08 12:04:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
-let g,b;
+let g;
+let b = [];
 let shooting;
 let notShooting;
 
 function setup() {
   createCanvas(400, 400);
   g = new Gun(200,350,50,50);
-  b = new Bottle(200,200,30,50);
+  for(var i =0 ; i < 5 ; i++){
+    b.push(new Bottle(50 + i*55,200,30,50)); 
+  }//end loop
+  
   notShooting = true;
   rect(0, 300, width, 20 );
 }
@@ -17,7 +21,10 @@ function draw() {
   rect(0, 230, width, 20 );
   g.display();
   g.update();
-  b.display();
+  for(var i =0 ; i <b.length ; i++){
+    b[i].display();
+  }
+  
   
 }
 function mouseClicked(){
@@ -29,11 +36,14 @@ function mouseClicked(){
 }
 
 function hitBottle(){
-  if(mouseX > b.x &&
-    mouseX < b.x + b.w &&
-    mouseY > b.y &&
-    mouseY < b.y + b.h){
+  for(var i =0 ; i < b.length ; i++){
+    if(mouseX > b[i].x &&
+    mouseX < b[i].x + b[i].w &&
+    mouseY > b[i].y &&
+    mouseY < b[i].y + b[i].h){
     bottleImage = loadImage("crackedBottle.png");
     
   }
+  } 
+  
 }//end hitBottle
