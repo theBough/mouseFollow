af46c138d28fd4fce45f5777eec7098a39776659
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-12-22 11:24:27 -0600

    Create waves.js

---
+++ waves.js
@@ -0,0 +1,19 @@
+function Waves(x,y,w,h){
+  
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.wavesImage = loadImage("waves.png");
+  
+  this.display = function(){
+    image(this.waves,this.x, this.y, this.w, this.h);
+  }//end display
+  
+  this.update = function (){
+    this.x -= 1
+    if(this.x < -400){
+      this.x = 400;
+    }//end if
+  }//end update
+}//end Gun
