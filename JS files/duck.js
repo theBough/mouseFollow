82e81fe82d056b2a3fd295cbb8d6f52de5b96dbd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-17 10:56:09 -0600

    Update duck.js

--- JS files/duck.js
+++ JS files/duck.js
@@ -5,15 +5,22 @@ function Duck(x,y,w,h){
   this.w = w;
   this.h = h;
   this.duckImage = loadImage("duckGood.png");
+  this.ceil = this.y +1;
+  this.floor = this.y - 10;
+  this.speed = 0.25
   
   this.display = function(){
     image(this.duckImage,this.x, this.y, this.w, this.h);
   }//end display
   this.update = function(){
-    this.x -= 3;
+    this.x -= 1;
     if(this.x < 0){
       this.x = width
     }//end if
+    this.y += this.speed;
+    if(this.y < this.floor || this.y > this.ceil){
+      this.speed *= -1;
+    }
   }//end update
   
 }//end Gun
